5f77c78f2acb97c3df5aeb566c4fa9d6a9641931
Henrique <henrique.dias@ua.pt>
2023-06-03 13:42:45 +0100

    teste de registo e login

--- bot-selenium/bot.py
+++ bot-selenium/bot.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-website = "http://127.0.0.1:5005/"
+website = "http://127.0.0.1:5005"
 driver = None
 
 def start_webdriver():
@@ -20,17 +20,32 @@ def voltar_home():
 def test_registo():
     driver.find_element(By.CLASS_NAME, "profile-icon").click()
     time.sleep(1)
-    driver.find_element(By.NAME, "name").send_keys("")
+    driver.find_element(By.NAME, "name_registo").send_keys("Henrique")
+    time.sleep(1)
+    driver.find_element(By.NAME, "email_registo").send_keys("henrique.dias@ua.pt")
+    time.sleep(1)
+    driver.find_element(By.NAME, "password_registo").send_keys("henrique")
+    time.sleep(1)
+    driver.find_element(By.ID, "submit_registo").click()
+    #ESTÁ A FUNCIONAR
+    
 
 def test_login():
     driver.find_element(By.CLASS_NAME, "profile-icon").click()
     time.sleep(1)
+    driver.find_element(By.NAME, "email").send_keys("joao@gmail.com")
+    time.sleep(1)
+    driver.find_element(By.NAME, "password").send_keys("1")
+    time.sleep(1)
+    driver.find_element(By.ID, "submit").click()
+    #ESTÁ A FUNCIONAR
+
 
 def main():
     start_webdriver()
-    test_registo()
-    voltar_home()
-    #test_login()
+    #test_registo()
+    #voltar_home()
+    test_login()
 
 if __name__ == "__main__":
     main()
